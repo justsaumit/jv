f8a86b6e6ec5764b2afd3760b49367290643dc37
Saumit Dinesan <justsaumit@protonmail.com>
2023-01-27 08:00:28 +0530

    slight corr in q7

--- q7.java
+++ q7.java
@@ -18,14 +18,14 @@ class Overload{
 public class q7{
     public static void main(String[]args){
         Overload obj1=new Overload();
-        System.out.println("For unparaterized constructor - ");
+        System.out.println("For unparameterized constructor - ");
         obj1.printElements();
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter value of x for parameterized constructor: ");
         int x=sc.nextInt();
         System.out.println("Enter value of y for parameterized constructor: ");
         int y=sc.nextInt();
-        System.out.println("For parameterized constructor and overloaded constructor- ");
+        System.out.println("For parameterized and overloaded constructor - ");
         Overload obj2=new Overload(x,y);
         obj2.printElements();
     }
