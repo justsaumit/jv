5d98b3eac4d52f9b8194462b59f81384fa1b343c
Saumit Dinesan <justsaumit@protonmail.com>
2023-02-10 01:19:07 +0530

    +comments in q12

--- Lab05/q12.java
+++ Lab05/q12.java
@@ -1,28 +1,29 @@
 // Q.12 Write a java program for Bank class using Method Overriding.
 import java.util.Scanner;
 
+//Base class Bank
 class Bank {
     public double getRateofInterest(){
-        return 8.50;
+        return 6.00;
     }
 }
 
+//subclassses V
+class Citibank extends Bank {
+    public double getRateofInterest(){  //overriding
+        return 7.25;
+    }
 class HDFC extends Bank {
-    public double getRateofInterest(){
+    public double getRateofInterest(){ //overriding
         return 8.75;
     }
 }
-
 class ICICI extends Bank {
-    public double getRateofInterest(){
+    public double getRateofInterest(){  //overriding
         return 7.50;
     }
 }
 
-class Citibank extends Bank {
-    public double getRateofInterest(){
-        return 7.25;
-    }
 }
 
 
@@ -32,6 +33,7 @@ public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         outer:
         while (true){
+            b=new Bank();
             System.out.println("------------------------------------------------------");
             System.out.println("Which Bank's Rate of Interest would you like to know?");
             System.out.println("1) Default Bank"+"\n"+"2) Citibank"+"\n"+"3) HDFC Bank"+"\n"+"4) ICICI Bank"+"\n"+"5) End Program");
@@ -39,7 +41,6 @@ public static void main(String[] args){
             boolean repeat;
                 switch (opt) {
                     case 1:
-                        b=new Bank();
                         System.out.println("Default Bank's Rate of Interest: " + b.getRateofInterest());
                         break;
                     case 2:
@@ -51,7 +52,7 @@ public static void main(String[] args){
                         System.out.println("HDFC Bank's Rate of Interest: "+b.getRateofInterest());  
                         break;
                     case 4:
-                        b=new ICICI();  
+                        b=new ICICI();  //comment this to show method overriding is achieved
                         System.out.println("ICICI Bank's Rate of Interest: "+b.getRateofInterest());  
                         break;
                     case 5:
