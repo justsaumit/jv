70b7f459af5e1d23cad917b620bb4b2e9c6c0e47
Saumit Dinesan <justsaumit@protonmail.com>
2023-02-10 00:51:53 +0530

    q13 done

---
+++ Lab05/q13.java
@@ -0,0 +1,29 @@
+// Q.13 Write a java program to demonstrate Method overriding (use super keyword)
+
+class Parent{
+    int age=40;
+    public void intro(){
+        System.out.println("Hello! I'm Parent! My Age is "+age);
+    }
+}
+
+class Child extends Parent{
+    public void intro(){
+    int age=14;
+        System.out.println("Hello! I'm Child! My Age is "+age);
+    } //method override by child class
+
+     public void useParentIntroMethod(){
+        super.intro();
+    } //Preventing override using super keyword
+}
+
+class q13{
+    public static void main(String []args){
+        Parent p=new Parent();
+        p.intro();               //Base class method works on calling itself
+        Child c=new Child();
+        c.intro();                //On calling show method the child class overrides the Parent's metho        c.useParentIntroMethod(); //By using super keyword we are now able to get result of parents method from child class
+    }
+    
+}
